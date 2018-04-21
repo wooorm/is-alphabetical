d7615e1dbefb6eeeea3d6d07a1a260ef9a535c1d
Titus Wormer <tituswormer@gmail.com>
2018-04-21 11:54:58 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var alphabetical = require('.');
+var test = require('tape')
+var alphabetical = require('.')
 
-test('alphabetical(character)', function (t) {
-  t.ok(alphabetical('a'));
-  t.ok(alphabetical('Z'));
-  t.ok(alphabetical('a'.charCodeAt(0)));
-  t.notOk(alphabetical('0'));
-  t.notOk(alphabetical('a'.charCodeAt(0) - 1));
-  t.notOk(alphabetical('z'.charCodeAt(0) + 1));
-  t.notOk(alphabetical('A'.charCodeAt(0) - 1));
-  t.notOk(alphabetical('Z'.charCodeAt(0) + 1));
-  t.notOk(alphabetical('0'.charCodeAt(0)));
-  t.notOk(alphabetical('💩'));
+test('alphabetical(character)', function(t) {
+  t.ok(alphabetical('a'))
+  t.ok(alphabetical('Z'))
+  t.ok(alphabetical('a'.charCodeAt(0)))
+  t.notOk(alphabetical('0'))
+  t.notOk(alphabetical('a'.charCodeAt(0) - 1))
+  t.notOk(alphabetical('z'.charCodeAt(0) + 1))
+  t.notOk(alphabetical('A'.charCodeAt(0) - 1))
+  t.notOk(alphabetical('Z'.charCodeAt(0) + 1))
+  t.notOk(alphabetical('0'.charCodeAt(0)))
+  t.notOk(alphabetical('💩'))
 
-  t.end();
-});
+  t.end()
+})
