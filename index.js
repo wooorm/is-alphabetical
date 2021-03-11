b9c00f17d5b22a16a5ff78881b0f5d229d832b9d
Titus Wormer <tituswormer@gmail.com>
2021-03-11 09:33:50 +0100

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// Check if the given character code, or the character code at the first
-// character, is alphabetical.
+/**
+ * Check if the given character code, or the character code at the first
+ * character, is alphabetical.
+ *
+ * @param {string|number} character
+ * @returns {boolean} Whether `character` is alphabetical.
+ */
 export function isAlphabetical(character) {
   var code = typeof character === 'string' ? character.charCodeAt(0) : character
 
