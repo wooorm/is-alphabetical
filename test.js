bb23728212603b9f6cb8e5b4213d8d84d6da3486
Titus Wormer <tituswormer@gmail.com>
2022-11-23 12:23:34 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {isAlphabetical} from './index.js'
 
-test('isAlphabetical(character)', function (t) {
+test('isAlphabetical(character)', function () {
   const _0 = 0x30
   const A = 0x41
   const Z = 0x5a
   const a = 0x61
   const z = 0x7a
-  t.ok(isAlphabetical('a'))
-  t.ok(isAlphabetical('Z'))
-  t.ok(isAlphabetical(a))
-  t.notOk(isAlphabetical('0'))
-  t.notOk(isAlphabetical(a - 1))
-  t.notOk(isAlphabetical(z + 1))
-  t.notOk(isAlphabetical(A - 1))
-  t.notOk(isAlphabetical(Z + 1))
-  t.notOk(isAlphabetical(_0))
-  t.notOk(isAlphabetical('💩'))
 
-  t.end()
+  assert.ok(isAlphabetical('a'))
+  assert.ok(isAlphabetical('Z'))
+  assert.ok(isAlphabetical(a))
+  assert.ok(!isAlphabetical('0'))
+  assert.ok(!isAlphabetical(a - 1))
+  assert.ok(!isAlphabetical(z + 1))
+  assert.ok(!isAlphabetical(A - 1))
+  assert.ok(!isAlphabetical(Z + 1))
+  assert.ok(!isAlphabetical(_0))
+  assert.ok(!isAlphabetical('💩'))
 })
